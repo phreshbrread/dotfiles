094daa142a7a4554324a2d08e0bbfb231a8f26e8
phreshbrread <crustydog9@gmail.com>
2022-02-22 22:25:54 +1100

    Update configs

--- .config/fish/functions/backupdotfiles.fish
+++ .config/fish/functions/backupdotfiles.fish
@@ -2,7 +2,6 @@ function backupdotfiles
 cd ~/dotfiles/.config
 cp -r /home/brad/.config/i3 (pwd)
 cp -r /home/brad/.config/fish (pwd)
-cp -r /home/brad/.config/kitty (pwd)
 cp -r /home/brad/.config/picom (pwd)
 cp -r /home/brad/.config/polybar (pwd)
 cp -r /home/brad/.config/alacritty (pwd)
