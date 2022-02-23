6415192a70fa2875ba7b7a2f65dd48fb4e181ba2
phreshbrread <crustydog9@gmail.com>
2022-02-23 14:58:02 +1100

    Update configs

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-yay -S --needed alacritty fish i3-gaps picom polybar nitrogen brightnessctl xautolock rofi thunar
+yay -S --needed alacritty fish i3-gaps picom polybar nitrogen brightnessctl xautolock rofi thunar yaru-colours-icon-theme
 
 cp -r .config/ ~/
