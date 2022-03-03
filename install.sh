14d2fc177c86299d28f9df1f77d554a857498d90
Brad <crustydog9@gmail.com>
2022-03-03 17:05:30 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-yay -S --needed alacritty fish i3-gaps picom polybar nitrogen brightnessctl xautolock rofi thunar yaru-colors-icon-theme beautyline sweet-gtk-theme-dark google-chrome exa fish arandr
+yay -S --needed alacritty fish i3-gaps picom polybar nitrogen brightnessctl xautolock rofi thunar yaru-colors-icon-theme beautyline sweet-gtk-theme-dark google-chrome exa fish arandr neofetch
 
 cp -r .config/ ~/
