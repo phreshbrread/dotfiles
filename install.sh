e1f05ef01f343b8407d5b621b16e6e05b9bd749e
Brad <crustydog9@gmail.com>
2022-03-13 15:03:48 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+while true
+do
+      read -r -p "Do you want to install the Yay AUR helper? [Y/n] " input
+ 
+      case $input in
+            [yY][eE][sS]|[yY])
+                  sudo pacman -S --needed git base-devel
+                  git clone https://aur.archlinux.org/yay.git
+                  cd yay
+                  makepkg -si
+                  
+                  break
+                  ;;
+            [nN][oO]|[nN])
+                  break
+                  ;;
+            *)
+                  echo "Invalid input..."
+                  ;;
+      esac      
+done
+
 yay -S --needed alacritty fish i3-gaps picom polybar nitrogen brightnessctl xautolock rofi thunar beautyline sweet-gtk-theme-dark google-chrome exa fish arandr neofetch lxappearance nordic-darker-theme papirus-nord
 
 cp -r .config/ ~/
