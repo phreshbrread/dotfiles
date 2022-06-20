8e7a45c64f592792dd26edce5046cfabf5456f66
phreshbrread <crustydog9@gmail.com>
2022-06-20 15:15:37 +1000

    Add files via upload

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ while true; do
       esac
 done
 
-yay -S --needed ttf-font-awesome alacritty fish i3-gaps i3status i3lock picom polybar onedriver nitrogen brightnessctl xautolock rofi nemo brave-bin exa fish arandr neofetch lxappearance qbittorrent dmenu xterm xpad network-manager-applet polkit-gnome baobab bat gnome-themes-extra lightdm-gtk-greeter lightdm-gtk-greeter-settings flameshot ghex github-cli gparted qt5ct tor torbrowser-launcher torsocks adwaita-qt5
+yay -S --needed ttf-font-awesome pacman-contrib alacritty fish i3-gaps i3status i3lock picom polybar onedriver nitrogen brightnessctl xautolock rofi nemo brave-bin exa fish arandr neofetch lxappearance qbittorrent dmenu xterm xpad network-manager-applet polkit-gnome baobab bat gnome-themes-extra lightdm-gtk-greeter lightdm-gtk-greeter-settings flameshot ghex github-cli gparted qt5ct tor torbrowser-launcher torsocks adwaita-qt5
 
 cd ~/
 mkdir -p Desktop Documents Downloads Pictures Videos Music
