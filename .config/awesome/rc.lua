b67022c2a1af1e3014a099169c11bba98d3e3837
phreshbrread <crustydog9@gmail.com>
2022-09-24 10:00:56 +1000

    Add files via upload

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -222,7 +222,6 @@ awful.screen.connect_for_each_screen(function(s)
             separator,
             BAT0,
             mytextclock,
-            --audio_widget(),
             wibox.widget.systray(),
             s.mylayoutbox,
         },
