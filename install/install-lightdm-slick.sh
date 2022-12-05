9155975a949b6b556d0e4a44c4acc8a9dbfc5242
phreshbrread <crustydog9@gmail.com>
2022-12-06 00:38:03 +1100

    Add files via upload

--- install/install-lightdm-slick.sh
+++ install/install-lightdm-slick.sh
@@ -3,4 +3,3 @@
 sudo pacman -S --needed lightdm-slick-greeter lightdm-settings
 
 sudo sed -i 's/#greeter-session=example-gtk-greeter/greeter-session=lightdm-slick-greeter/g' /etc/lightdm/lightdm.conf
-
--- install/install.sh
+++ install/install.sh
@@ -1,21 +1,3 @@
 #!/bin/bash
 
 yay -S --needed awesome pa-applet-git ttf-hack font-manager ttf-font-awesome flameshot pacman-contrib alacritty picom polybar onedrive nitrogen brightnessctl rofi dolphin firefox exa fish arandr neofetch lxappearance qbittorrent xterm network-manager-applet lxsession baobab bat gnome-themes-extra lightdm-gtk-greeter lightdm-gtk-greeter-settings ghex github-cli gparted qt5ct-kde tor torbrowser-launcher torsocks blueman dex pa-applet-git breeze breeze-gtk
-
-while true; do
-      read -r -p "Enable LightDM? [Y/n] " input
-
-      case $input in
-      [yY][eE][sS] | [yY])
-            sudo systemctl enable lightdm.service
-
-            break
-            ;;
-      [nN][oO] | [nN])
-            break
-            ;;
-      *)
-            echo "Invalid input..."
-            ;;
-      esac
-done
