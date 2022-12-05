1fd4d34f2fd3a21a9a121e542d6a476a29bee057
PhreshBrread <crustydog9@gmail.com>
2022-12-05 22:14:22 +1100

    Add files via upload

--- install/install.sh
+++ install/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yay -S --needed awesome pa-applet-git ttf-hack font-manager ttf-font-awesome flameshot pacman-contrib alacritty picom polybar onedrive nitrogen brightnessctl rofi dolphin firefox exa fish arandr neofetch lxappearance qbittorrent xterm network-manager-applet polkit-gnome baobab bat gnome-themes-extra lightdm-gtk-greeter lightdm-gtk-greeter-settings ghex github-cli gparted qt5ct-kde tor torbrowser-launcher torsocks blueman dex pa-applet-git breeze breeze-gtk
+yay -S --needed awesome pa-applet-git ttf-hack font-manager ttf-font-awesome flameshot pacman-contrib alacritty picom polybar onedrive nitrogen brightnessctl rofi dolphin firefox exa fish arandr neofetch lxappearance qbittorrent xterm network-manager-applet lxsession baobab bat gnome-themes-extra lightdm-gtk-greeter lightdm-gtk-greeter-settings ghex github-cli gparted qt5ct-kde tor torbrowser-launcher torsocks blueman dex pa-applet-git breeze breeze-gtk
 
 while true; do
       read -r -p "Enable LightDM? [Y/n] " input
