367b0019c83b6c4e787f1a31b1c1910929484e62
PhreshBrread <crustydog9@gmail.com>
2025-06-08 14:08:35 +1000

    Formatting changes

--- nix/home.nix
+++ nix/home.nix
@@ -5,24 +5,26 @@
 { config, pkgs, ... }:
 
 {
-  home.username = "brad";
-  home.homeDirectory = "/home/brad";
+  home = {
+    username = "brad";
+    homeDirectory = "/home/brad";
+
+    # This value determines the home Manager release that your
+    # configuration is compatible with. This helps avoid breakage
+    # when a new home Manager release introduces backwards
+    # incompatible changes.
+    #
+    # You can update home Manager without changing this value. See
+    # the home Manager release notes for a list of state version
+    # changes in each release.
+    stateVersion = "24.11";
+  };
 
   programs.git = {
     enable = true;
     userName = "PhreshBrread";
     userEmail = "crustydog9@gmail.com";
   };
-
-  # This value determines the home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update home Manager without changing this value. See
-  # the home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "24.11";
 
   # Let home Manager install and manage itself.
   programs.home-manager.enable = true;
