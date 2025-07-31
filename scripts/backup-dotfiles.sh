17cba59f0bcb6b76f104948c9eec5a91c7f86ee4
Brad <crustydog9@gmail.com>
2025-07-31 20:49:59 +1000

    Remove unused scripts

--- scripts/backup-dotfiles.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-cp -r ~/.config/alacritty/ ~/.config/hypr/ ~/.config/fish/ ~/.config/waybar/ ~/dotfiles/config/
--- scripts/steam-console-mode.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-killall steam steamwebhelper
-
-gamescope -O DP-1 --display-index 0 -e -f -C --adaptive-sync -h 1440 -H 1080 -r 144 -- /usr/bin/steam -tenfoot &
