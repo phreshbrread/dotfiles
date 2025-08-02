1c8bfc4340fcf81da6eddf9828f5cd7b2d05839d
Brad <crustydog9@gmail.com>
2025-08-02 13:48:01 +1000

    Change some docker stuff

--- nix/desktop/configuration.nix
+++ nix/desktop/configuration.nix
@@ -91,6 +91,8 @@
     extraGroups = [
       "networkmanager"
       "wheel"
+      "video"
+      "render"
     ];
   };
 
--- nix/desktop/server-specific.nix
+++ nix/desktop/server-specific.nix
@@ -8,11 +8,15 @@
   # Docker
   virtualisation.docker = {
     enable = true;
-    rootless = {
-      enable = true;
-      setSocketVariable = true;
+    daemon.settings = {
+      userland-proxy = false;
+      experimental = true;
+      metrics-addr = "0.0.0.0:9323";
+      ipv6 = true;
+      fixed-cidr-v6 = "fd00::/80";
     };
   };
+  users.users.brad.extraGroups = [ "docker" ];
 
   # SSH
   services.openssh = {
