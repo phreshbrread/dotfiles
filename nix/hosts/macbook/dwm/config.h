64c274afca9bce258f7362882284cfb78aa93064
Brad <crustydog9@gmail.com>
2025-10-24 14:17:19 +1100

    Tiny formatting edit

--- nix/hosts/macbook/dwm/config.h
+++ nix/hosts/macbook/dwm/config.h
@@ -110,15 +110,15 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
+	  TAGKEYS(                      XK_1,                      0)
+	  TAGKEYS(                      XK_2,                      1)
+	  TAGKEYS(                      XK_3,                      2)
+	  TAGKEYS(                      XK_4,                      3)
+	  TAGKEYS(                      XK_5,                      4)
+	  TAGKEYS(                      XK_6,                      5)
+	  TAGKEYS(                      XK_7,                      6)
+	  TAGKEYS(                      XK_8,                      7)
+	  TAGKEYS(                      XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_m,      quit,           {0} },
 	{ 0,            XF86XK_MonBrightnessUp,    spawn,          {.v = incbrightness } },
 	{ 0,            XF86XK_MonBrightnessDown,  spawn,          {.v = decbrightness } },
