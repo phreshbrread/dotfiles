fe415c6356dd71ac3d37ff8ad77aa9aaed3a9d7b
Brad <crustydog9@gmail.com>
2025-12-02 13:43:16 +1100

    Set home-manager version

--- nix/flake.nix
+++ nix/flake.nix
@@ -14,7 +14,7 @@
     catppuccin.url = "github:catppuccin/nix";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
