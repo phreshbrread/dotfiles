1a7a172008f918e6eedf2c87da25c807643e851c
Brad <crustydog9@gmail.com>
2025-12-10 08:55:29 +1100

    Switch some more stuff to lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,3 +1,9 @@
+-----------------
+-- NVIM CONFIG --
+-----------------
+
+-- Plugins are installed/managed by Nix, but are configured/loaded here
+
 -- Set options
 vim.o.termguicolors = true
 vim.o.number = true
@@ -8,9 +14,7 @@ vim.o.syntax = 'on'
 vim.o.shiftwidth = 4
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
-vim.cmd('filetype plugin indent on')
-vim.cmd('colorscheme tokyonight')
-vim.cmd('hi Normal guibg=NONE ctermbg=NONE') -- Transparent background
+vim.cmd('filetype plugin indent on') -- TODO switch this to lua equivalent
 
 -- Custom keybinds
 vim.keymap.set('n', '<C-p>', ':Telescope find_files<CR>')
@@ -19,7 +23,10 @@ vim.keymap.set('n', '<C-p>', ':Telescope find_files<CR>')
 require('lualine').setup()
 require('nvim-highlight-colors').setup({})
 
--- TODO switch vim.cmd to lua
 -- TODO separate into options and keybinds files
-    -- require('config.options') -- [./lua/options.lua]
-    -- require('config.keybinds') -- [./lua/keybinds.lua]
+-- require('config.options') -- [./lua/options.lua]
+-- require('config.keybinds') -- [./lua/keybinds.lua]
+
+-- Set colours
+vim.cmd.colorscheme "tokyonight"
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" }) -- Transparent background
