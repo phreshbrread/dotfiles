59ebb8612f74f377bbfd25a25c0334a5ef5582d4
Brad <crustydog9@gmail.com>
2025-12-10 20:46:46 +1100

    Comments

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -1,3 +1,11 @@
+-- TODO:
+-- Change font
+-- Use picom
+-- Change colours
+-- Battery widget
+-- App tray
+-- Remove top right menu
+
 -- If LuaRocks is installed, make sure that packages installed through it are
 -- found (e.g. lgi). If LuaRocks is not installed, do nothing.
 pcall(require, "luarocks.loader")
