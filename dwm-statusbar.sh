fa963bc2171341c0f83865e87481e1bf72453976
Brad <crustydog9@gmail.com>
2025-12-10 17:59:25 +1100

    Fixup dwm status bar

--- dwm-statusbar.sh
+++ dwm-statusbar.sh
@@ -1,25 +1,27 @@
 #! /usr/bin/env bash
 
-DATE=$(date +"%F")
-TIME=$(date +"%R")
-BAT_STATUS=$(cat /sys/class/power_supply/BAT0/status)
-BAT_PERCENT=$(cat /sys/class/power_supply/BAT0/capacity)%
-VOLUME=$(wpctl get-volume @DEFAULT_AUDIO_SINK@)
-BRIGHTNESS_PERCENT=$(brightnessctl g)%
+while [ true ]; do
+    DATE=$(date +"%F")
+    TIME=$(date +"%R")
+    BAT_STATUS=$(cat /sys/class/power_supply/BAT0/status)
+    BAT_PERCENT=$(cat /sys/class/power_supply/BAT0/capacity)%
+    VOLUME=$(wpctl get-volume @DEFAULT_AUDIO_SINK@)
+    BRIGHTNESS_PERCENT=$(brightnessctl g)%
 
-if [ $BAT_STATUS == "Charging" ]; then
-    BAT_STATUS_ICON="↑"
-elif [ $BAT_STATUS == "Discharging" ]; then
-    BAT_STATUS_ICON="↓"
-else
-    BAT_STATUS_ICON=""
-fi
+    if [ $BAT_STATUS == "Charging" ]; then
+        BAT_STATUS_ICON="↑"
+    elif [ $BAT_STATUS == "Discharging" ]; then
+        BAT_STATUS_ICON="↓"
+    else
+        BAT_STATUS_ICON=""
+    fi
 
-if [[ $VOLUME == *"MUTED"* ]]; then
-    VOLUME="MUTE"
-else
-    VOLUME=$(echo $VOLUME | awk -F': ' '{printf "%.0f%%\n", $2 * 100}')
-fi
+    if [[ $VOLUME == *"MUTED"* ]]; then
+        VOLUME="MUTE"
+    else
+        VOLUME=$(echo $VOLUME | awk -F': ' '{printf "%.0f%%\n", $2 * 100}')
+    fi
 
-xsetroot -name "[SCRN: $BRIGHTNESS_PERCENT] [VOL: $VOLUME] [BAT: $BAT_PERCENT$BAT_STATUS_ICON] [$TIME] [$DATE]"
-sleep 1s
+    xsetroot -name "[SCRN: $BRIGHTNESS_PERCENT] [VOL: $VOLUME] [BAT: $BAT_PERCENT$BAT_STATUS_ICON] [$TIME] [$DATE]"
+    sleep 1s
+done
--- nix/dwm/config.h
+++ nix/dwm/config.h
@@ -41,6 +41,8 @@ static const char *colors[][3]           = {
 static const char *const autostart[] = {
 	"nitrogen", "--restore", NULL,
 	"nm-applet", NULL,
+	"picom", "--backend", "glx", NULL,
+	"sh", "-c", "$HOME/dotfiles/dwm-statusbar.sh", NULL,
 	NULL /* terminate */
 };
 
@@ -58,7 +60,7 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.6; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
@@ -83,7 +85,7 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static char dmenumon[2]            = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[]      = { "dmenu_run", "-i", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]       = { "alacritty", NULL };
 static const char *incbrightness[] = { "brightnessctl", "set", "5%+", NULL };
