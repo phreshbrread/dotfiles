cafebf7ced1501a7ff0906ceea9d9a22086305a3
Brad <crustydog9@gmail.com>
2026-05-05 13:28:17 +1000

    Begin migrating hyprland to lua

---
+++ config/hypr/hyprland.lua
@@ -0,0 +1,12 @@
+---------------------------
+-- [[ HYPRLAND CONFIG ]] --
+---------------------------
+
+
+-- [[ MONITORS ]] --
+hl.monitor({ output = "DP-1", mode = "1920x1080@144", position = "0x0", scale = 1 })        -- Primary (Gigabyte)
+hl.monitor({ output = "DP-2", mode = "1920x1080@144", position = "0x0", scale = 1 })        -- Primary (Gigaybte)
+hl.monitor({ output = "HDMI-A-1", mode = "1920x1080@60", position = "1920x0", scale = 1 })  -- Secondary (BenQ)
+hl.monitor({ output = "eDP-1", mode = "highres", position = "0x0", scale = 2 })             -- Macbook
+hl.monitor({ output = "Virtual-1", mode = "1920x1080", position = "0x0", scale = 1 })       -- Virtual Machine
+hl.monitor({ output = "WL-1", mode = "1366x768", position = "0x0", scale = 1 })             -- Nested Session
