28439e29f1195ae39db0e67e7bf4f555716e6a41
Brad <crustydog9@gmail.com>
2026-05-05 13:33:15 +1000

    Minor edit

--- config/hypr/hyprland.lua
+++ config/hypr/hyprland.lua
@@ -4,9 +4,20 @@
 
 
 -- [[ MONITORS ]] --
-hl.monitor({ output = "DP-1", mode = "1920x1080@144", position = "0x0", scale = 1 })        -- Primary (Gigabyte)
-hl.monitor({ output = "DP-2", mode = "1920x1080@144", position = "0x0", scale = 1 })        -- Primary (Gigaybte)
-hl.monitor({ output = "HDMI-A-1", mode = "1920x1080@60", position = "1920x0", scale = 1 })  -- Secondary (BenQ)
-hl.monitor({ output = "eDP-1", mode = "highres", position = "0x0", scale = 2 })             -- Macbook
-hl.monitor({ output = "Virtual-1", mode = "1920x1080", position = "0x0", scale = 1 })       -- Virtual Machine
-hl.monitor({ output = "WL-1", mode = "1366x768", position = "0x0", scale = 1 })             -- Nested Session
+-- Primary (Gigabyte)
+hl.monitor({ output = "DP-1", mode = "1920x1080@144", position = "0x0", scale = 1 })
+
+-- Primary (Gigaybte)
+hl.monitor({ output = "DP-2", mode = "1920x1080@144", position = "0x0", scale = 1 })
+
+-- Secondary (BenQ)
+hl.monitor({ output = "HDMI-A-1", mode = "1920x1080@60", position = "1920x0", scale = 1 })
+
+-- Macbook
+hl.monitor({ output = "desc:Apple Computer Inc Color LCD", mode = "highres", position = "0x0", scale = 2 })
+
+-- Virtual Machine
+hl.monitor({ output = "Virtual-1", mode = "1920x1080", position = "0x0", scale = 1 })
+
+-- Nested Session
+hl.monitor({ output = "WL-1", mode = "1366x768", position = "0x0", scale = 1 })
