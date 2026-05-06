0517971fce6af524473d3f795c2e0da0826f8fcd
Brad <crustydog9@gmail.com>
2026-05-06 21:31:56 +1000

    Desktop stuff

--- config/hypr/hyprland.lua
+++ config/hypr/hyprland.lua
@@ -5,7 +5,7 @@
 
 -- [[ MONITORS ]] --
 -- Primary (Gigabyte)
-hl.monitor({ output = "DP-1", mode = "1920x1080@144", position = "0x0", scale = 1 })
+hl.monitor({ output = "desc: GIGA-BYTE TECHNOLOGY CO. LTD. G27F 21282B001333", mode = "1920x1080@144", position = "0x0", scale = 1 })
 
 -- Primary (Gigaybte)
 hl.monitor({ output = "DP-2", mode = "1920x1080@144", position = "0x0", scale = 1 })
