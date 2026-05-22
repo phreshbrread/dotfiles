1e5efcc78c67e1c68693960fe5162e2448b7565e
PhreshBrread <crustydog9@gmail.com>
2026-05-23 02:00:16 +1000

    Nvim keybind

--- config/nvim/lua/keybinds.lua
+++ config/nvim/lua/keybinds.lua
@@ -6,3 +6,6 @@ vim.keymap.set('n', '<C-l>', ':Telescope live_grep<CR>')
 
 -- LSP diagnostics
 vim.keymap.set('n', 'gl', vim.diagnostic.open_float)
+
+-- Auto indent in place
+vim.keymap.set('n', '<C-f>', 'mzgg=G`z')
