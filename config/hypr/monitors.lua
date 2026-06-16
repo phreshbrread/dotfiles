852b9479a3d41079594d771e015fc9c196b28730
PhreshBrread <crustydog9@gmail.com>
2026-06-16 13:49:58 +1000

    Continue porting hypr config to lua

---
+++ config/hypr/monitors.lua
@@ -0,0 +1,22 @@
+-- See https://wiki.hypr.land/Configuring/Basics/Monitors/
+hl.monitor({
+  output = "desc:GIGA-BYTE TECHNOLOGY CO. LTD. G27F 21282B001333",
+  mode = "1920x1080@144",
+  position = "0x0",
+  scale = 1,
+  vrr = true,
+})
+
+hl.monitor({
+  output = "desc: BNQ BenQ RL2455 65F03137SL0",
+  mode = "1920x1080@60",
+  position = "1920x0",
+  scale = 1,
+})
+
+hl.monitor({
+  output = "desc:Apple Computer Inc Color LCD",
+  mode = "preferred",
+  position = "auto",
+  scale = 2,
+})
