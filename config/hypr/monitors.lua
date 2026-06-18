4280de8878f23061e38fd0976c61f3acaa15d5f3
PhreshBrread <crustydog9@gmail.com>
2026-06-18 20:13:38 +1000

    Bind workspaces to monitors

--- config/hypr/monitors.lua
+++ config/hypr/monitors.lua
@@ -24,3 +24,19 @@ hl.monitor({
   position = "auto",
   scale = 2,
 })
+
+------------------------
+-- WORKSPACE BINDINGS --
+------------------------
+
+-- 1-5 -> Gigabyte
+for w = 1, 5 do
+    hl.workspace_rule({ workspace = tostring(w), monitor = "desc:GIGA-BYTE TECHNOLOGY CO. LTD. G27F 21282B001333", default = (w == 1) })
+end
+
+-- 6-10 -> BenQ
+for w = 6, 10 do
+    local ws_name = (w == 10) and "0" or tostring(w)
+    hl.workspace_rule({ workspace = ws_name, monitor = "desc:BNQ BenQ RL2455 65F03137SL0", default = (w == 6) })
+end
+
