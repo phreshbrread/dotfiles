443bb176bcbea101d018e7b8c5f37cacabb4387a
PhreshBrread <crustydog9@gmail.com>
2026-06-29 21:09:05 +1000

    Fix autostarts

--- scripts/autostarts.sh
+++ scripts/autostarts.sh
@@ -1,14 +1,13 @@
-#! /bin/sh
+#! /usr/bin/env bash
 
 # For all WMs
-pgrep nm-applet          || nm-applet &
-pgrep qpwgraph           || qpwgraph &
-pgrep lxqt-notificationd || lxqt-notificationd &
-pgrep openrgb            || openrgb &
-pgrep kdeconnectd        || kdeconnectd &
+pgrep nm-applet             || nm-applet          &
+pgrep qpwgraph              || qpwgraph           &
+pgrep -f lxqt-notificationd || lxqt-notificationd &
+pgrep openrgb               || openrgb            &
+pgrep kdeconnectd           || kdeconnectd        &
 
-# Import theme env
-systemctl --user import-environment QT_QPA_PLATFORMTHEME QT_PLUGIN_PATH
+systemctl --user import-environment QT_QPA_PLATFORMTHEME QT_PLUGIN_PATH & # Import theme env
 
 # X only
 # TODO:
@@ -16,25 +15,25 @@ systemctl --user import-environment QT_QPA_PLATFORMTHEME QT_PLUGIN_PATH
 # Clipboard manager
 # D-bus
 if [ "$XDG_SESSION_TYPE" == "x11" ]; then
-    nitrogen --restore
-    picom    --backend glx
+    nitrogen --restore     &
+    picom    --backend glx &
 fi
 
 # Wayland only
 if [ "$XDG_SESSION_TYPE" == "wayland" ]; then
     pgrep waybar || waybar &
-    waypaper --restore
+    waypaper --restore     &
 
     # Clipboard manager
-    wl-paste --type text --watch cliphist store # Stores only text data
-    wl-paste --type image --watch cliphist store # Stores only image datafi
+    pgrep wl-paste || wl-paste --type text --watch cliphist store  & # Stores only text data
+    pgrep wl-paste || wl-paste --type image --watch cliphist store & # Stores only image datafi
 
     # D-bus
-    dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+    dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP &
 fi
 
 # Hyprland specific
 if [ "$XDG_SESSION_DESKTOP" == "Hyprland" ]; then
     hyprsunset &
-    systemctl --user start hyprpolkitagent
+    systemctl --user start hyprpolkitagent &
 fi
