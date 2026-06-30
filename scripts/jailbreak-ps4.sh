374a7d56734cb13a82b9018d9cf18b4d9938dcc0
PhreshBrread <crustydog9@gmail.com>
2026-06-30 13:16:50 +1000

    Add script

---
+++ scripts/jailbreak-ps4.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/env bash
+
+/mass-storage/_PS4/pppwn-cpp/pppwn -i enp8s0 --fw 1100 --stage1 "/mass-storage/_PS4/pppwn-cpp/stage1.bin" --stage2 "/mass-storage/_PS4/pppwn-cpp/stage2.bin" -a
