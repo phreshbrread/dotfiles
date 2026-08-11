2093e4fddd6ea548ad67e60ff167cce07028b1eb
PhreshBrread <crustydog9@gmail.com>
2026-08-11 19:26:27 +1000

    Properly shut down VMs

--- nix/nixModules/submodules/vm-module.nix
+++ nix/nixModules/submodules/vm-module.nix
@@ -15,13 +15,13 @@
   };
 
   config = lib.mkIf config.vm-module.enable {
-    programs.virt-manager.enable = true;
+    programs.virt-manager.enable  = true;
     users.groups.libvirtd.members = [ "brad" ];
+    systemd.services.libvirt-guests.environment.ON_SHUTDOWN = lib.mkForce "shutdown";
     virtualisation = {
       spiceUSBRedirection.enable = true;
       libvirtd = {
         enable = true;
-        onShutdown = "shutdown";
         qemu.vhostUserPackages = with pkgs; [ virtiofsd ];
       };
     };
