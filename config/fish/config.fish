0576e7977c8f725f7d47a733de1194f201d6447a
PhreshBrread <crustydog9@gmail.com>
2026-09-21 12:48:08 +1000

    Add fish aliases to config.fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,3 +7,17 @@ set fish_greeting
 starship init fish | source
 
 direnv hook fish | source
+
+# Aliases
+alias cat            "bat"
+alias x              "exit"
+alias ls             "eza -algh --group-directories-first --icons=always";
+alias cls            "clear";
+alias tmux-mc        "tmux a -t minecraft-server";
+alias sudoedit       "doas rnano";
+alias install-date   "stat / | grep Birth";
+alias tmux-terraria  "tmux a -t terraria-server";
+alias nv             "nvim";
+alias ff             "fastfetch";
+alias cat            "bat";
+alias gpr            "git pull --rebase";
